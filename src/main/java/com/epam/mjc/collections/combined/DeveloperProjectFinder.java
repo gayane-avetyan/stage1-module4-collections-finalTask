c087120b87f42f3f21c7af108599f56f398a5e68
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-09 01:21:22 +0400

    comparator added

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -12,6 +12,19 @@ public class DeveloperProjectFinder {
                 devProjects.add(el.getKey());
         }
 
+        devProjects.sort(new Comparator<>() {
+            @Override
+            public int compare(String o1, String o2) {
+
+                if(o1.length() > o2.length())
+                    return -1;
+                else if(o1.length() < o2.length())
+                    return 1;
+                else
+                    return o2.compareTo(o1);
+            }
+        });
+
         return devProjects;
     }
 }
